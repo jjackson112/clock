b6e06a43fb924af15eb7fd8e8181fa3cba316132
J. Jackson <jjackson11211@gmail.com>
2022-12-05 10:38:31 -0500

    Redo of all files

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const secondHand = document.querySelector('[data-second-hand]')
 
 function setClock() {
   const currentDate = new Date()
-
   const secondsRatio = currentDate.getSeconds() / 60
   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
   const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
